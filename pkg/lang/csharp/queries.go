e066a95af5b810ea25f1775f550755b900d024a5
gordon-klotho <gordon.goetz@klo.dev>
2023-01-30 20:31:22 -0500

    Update names from copy/paste

--- pkg/lang/csharp/queries.go
+++ pkg/lang/csharp/queries.go
@@ -5,7 +5,7 @@ import (
 	sitter "github.com/smacker/go-tree-sitter"
 )
 
-// DoQuery is a thin wrapper around `query.Exec` to use csharp as the Language.
+// DoQuery is a thin wrapper around `query.Exec` to use C# as the Language.
 func DoQuery(c *sitter.Node, q string) query.NextMatchFunc {
 	return query.Exec(Language, c, q)
 }
