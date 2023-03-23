10a45598b7478af8cde5616a12e6d2884ecc7c8a
Yuval Shavit <110620369+yuval-klotho@users.noreply.github.com>
2023-03-23 19:25:05 -0400

    v1 of the iac plugin (#394)
    The "main event" here is templatesCompiler. It takes in a graph
    (expected to be a DAG) of cloud resources, and translates each one into
    a `const varname = ....` statement. For now, the idea is they'd all go
    into a single `index.ts` -- but we can think about splitting those up
    later.

---
+++ pkg/infra/iac2/templates/docker_lambda/factory.ts
@@ -0,0 +1,33 @@
+import * as aws from '@pulumi/aws'
+import * as pulumi from '@pulumi/pulumi'
+import {LogGroup} from "@pulumi/aws/cloudwatch";
+import {Role} from "@pulumi/aws/iam";
+
+interface Args {
+    ExecUnitName: string,
+    // lambdaName: string,
+    // image: pulumi.Output<string>,
+    CloudwatchGroup: LogGroup,
+    // lambdaRole: Role,
+    // envVars: Record<string, pulumi.Output<string>>,
+    // dependsOn: []
+}
+
+// noinspection JSUnusedLocalSymbols
+function create(args: Args): aws.lambda.Function {
+    return new aws.lambda.Function(
+        args.ExecUnitName,
+        {
+            packageType: 'Image',
+            imageUri: "TODO-image-uri",//args.image,
+            role: "TODO-role",//args.lambdaRole.arn,
+            name: "TODO-lambda-name",//args.lambdaName,
+            tags: {
+                env: 'production',
+                service: args.ExecUnitName,
+            },
+        },
+        {
+            dependsOn: [args.CloudwatchGroup],
+        });
+}
