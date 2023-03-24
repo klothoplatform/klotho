189eb96eb32d0d64b9269f1467d3c49181925b97
jhsinger-klotho <111291520+jhsinger-klotho@users.noreply.github.com>
2023-03-24 11:16:50 -0400

    adding iam role and ecr tempaltes (#398)

---
+++ pkg/infra/iac2/templates/iam_role/factory.ts
@@ -0,0 +1,14 @@
+import * as aws from '@pulumi/aws'
+
+interface Args {
+    Name: string
+    AssumeRolePolicyDoc: string
+    ManagedPolicyArns: string[]
+}
+
+// noinspection JSUnusedLocalSymbols
+function create(args: Args): aws.iam.Role {
+    return new aws.iam.Role(args.Name, {
+        assumeRolePolicy: args.AssumeRolePolicyDoc,
+    })
+}
