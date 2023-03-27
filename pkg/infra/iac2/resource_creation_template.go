febb78d534f6bb89888d5958b9afacebd61875f1
Yuval Shavit <110620369+yuval-klotho@users.noreply.github.com>
2023-03-27 19:11:28 -0400

    documentation and cleanup (#407)
    Most of this is just adding godocs. As part of that, expose some fields
    (so that godoc picks them up) and move iac_templates.go ->
    resource_creation_template.go.
    
    Also an remove unused file (the stubs)

--- pkg/infra/iac2/resource_creation_template.go
+++ pkg/infra/iac2/resource_creation_template.go
@@ -17,15 +17,22 @@ import (
 
 type (
 	ResourceCreationSignature struct {
-		inputTypes map[string]string
-		outputType string
+		// InputTypes is the inputs that this template requires: a Go-struct reflection of the `interface Args` in
+		// the TypeScript source. The keys are the field names in the `Args` interface, and the values are their types.
+		InputTypes map[string]string
+		// OutputType is the type that the template's `create(...)` function returns
+		OutputType string
 	}
 
+	// ResourceCreationTemplate contains information about a parsed template
 	ResourceCreationTemplate struct {
 		ResourceCreationSignature
-		name               string
-		imports            map[string]struct{}
-		expressionTemplate string
+		// name is the template's name, which is only used for error reporting
+		name string
+		// Imports is a set of import statements that the template needs
+		Imports map[string]struct{}
+		// ExpressionTemplate is a Go-[text/template] for a TypeScript expression to generate a piece of infrastructure.
+		ExpressionTemplate string
 	}
 )
 
@@ -48,13 +55,26 @@ var (
 	findImportsQuery string
 )
 
+// ParseResourceCreationTemplate parses TypeScript file into a ResourceCreationTemplate, which TemplatesCompiler
+// can then use. It looks for three things within the TypeScript source:
+//
+//  1. an imports section, which become the ResourceCreationTemplate's `imports` field.
+//  2. an `interface Args`, which contains the inputs this template expects. Those turn into the
+//     ResourceCreationSignature's `inputTypes` map.
+//  3. a `function create(args: Args)`, which is expected to contain only a single `return` statement.
+//
+// The `create` function gets used in two ways:
+//
+//  1. Its return value becomes the ResourceCreationSignature `outputType`
+//  2. Its `return` expression becomes the ResourceCreationTemplate `expressionTemplate`. As part of this, any usage of
+//     `arg.Foo` will get turned into `{{.Foo}}` for use in a Go template.
 func ParseResourceCreationTemplate(name string, contents []byte) ResourceCreationTemplate {
 	node := parseFile(contents)
 
 	result := ResourceCreationTemplate{name: name}
 
 	// inputs
-	result.inputTypes = make(map[string]string)
+	result.InputTypes = make(map[string]string)
 	nextInput := doQuery(node, findArgsQuery)
 	for {
 		match, found := nextInput()
@@ -62,7 +82,7 @@ func ParseResourceCreationTemplate(name string, contents []byte) ResourceCreatio
 			break
 		}
 		inputName, inputType := match["property_name"].Content(), match["property_type"].Content()
-		result.inputTypes[inputName] = inputType
+		result.InputTypes[inputName] = inputType
 	}
 
 	// return type and expression
@@ -72,11 +92,11 @@ func ParseResourceCreationTemplate(name string, contents []byte) ResourceCreatio
 		// unexpected, since all inputs are from resources in the klotho binary
 		panic("couldn't find valid create() function")
 	}
-	result.outputType = create["return_type"].Content()
-	result.expressionTemplate = parameterizeArgs(create["return_body"].Content())
+	result.OutputType = create["return_type"].Content()
+	result.ExpressionTemplate = parameterizeArgs(create["return_body"].Content())
 
 	// imports
-	result.imports = make(map[string]struct{})
+	result.Imports = make(map[string]struct{})
 	importsQuery := doQuery(node, findImportsQuery)
 	for {
 		match, found := importsQuery()
@@ -87,7 +107,7 @@ func ParseResourceCreationTemplate(name string, contents []byte) ResourceCreatio
 		// Trim any trailing semicolons. This helps normalize imports, so that we don't include them twice if one file
 		// includes the semicolon and the other doesn't.
 		importLine = strings.TrimRight(importLine, ";")
-		result.imports[importLine] = struct{}{}
+		result.Imports[importLine] = struct{}{}
 	}
 
 	return result
@@ -119,7 +139,7 @@ func parseFile(contents []byte) *sitter.Node {
 }
 
 func (t ResourceCreationTemplate) RenderCreate(out io.Writer, inputs map[string]string) error {
-	tmpl, err := template.New("template").Parse(t.expressionTemplate)
+	tmpl, err := template.New("template").Parse(t.ExpressionTemplate)
 	if err != nil {
 		return errors.Wrapf(err, `while writing template for %s`, t.name)
 	}
