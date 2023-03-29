c272e740708fd62e99e64de0079ead1845b0e0d7
Jordan Singer <jordan.singer@klo.dev>
2023-03-29 18:25:19 -0500

    start docs and static unit

---
+++ pkg/provider/aws/static_unit_test.go
@@ -0,0 +1,125 @@
+package aws
+
+import (
+	"testing"
+
+	"github.com/klothoplatform/klotho/pkg/annotation"
+	"github.com/klothoplatform/klotho/pkg/config"
+	"github.com/klothoplatform/klotho/pkg/core"
+	"github.com/klothoplatform/klotho/pkg/graph"
+	"github.com/klothoplatform/klotho/pkg/provider/aws/resources/s3"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_GenerateStaticUnitResources(t *testing.T) {
+	unit := &core.StaticUnit{AnnotationKey: core.AnnotationKey{ID: "test", Capability: annotation.ExecutionUnitCapability}}
+	unit.AddSharedFile(&core.FileRef{FPath: "test/Shared"})
+	unit.AddStaticFile(&core.FileRef{FPath: "test/Static"})
+	bucket := s3.NewS3Bucket(unit, "test-app")
+	obj1 := s3.NewS3Object(bucket, "Shared", "test/Shared", "test/test/Shared")
+	obj2 := s3.NewS3Object(bucket, "Static", "test/Static", "test/test/Static")
+
+	type testResult struct {
+		nodes []core.Resource
+		deps  []graph.Edge[core.Resource]
+		err   bool
+	}
+	cases := []struct {
+		name          string
+		indexDocument string
+		cfg           config.Application
+		want          testResult
+	}{
+		{
+			name: "generate static unit with no index document",
+			cfg: config.Application{
+				AppName: "test-app",
+			},
+			want: testResult{
+				nodes: []core.Resource{
+					bucket, obj1, obj2,
+				},
+				deps: []graph.Edge[core.Resource]{
+					{
+						Source:      bucket,
+						Destination: obj1,
+					},
+					{
+						Source:      bucket,
+						Destination: obj2,
+					},
+				},
+			},
+		},
+		{
+			name: "generate static unit with index document as website",
+			cfg: config.Application{
+				AppName: "test-app",
+			},
+			indexDocument: "index.html",
+			want: testResult{
+				nodes: []core.Resource{
+					bucket, obj1, obj2,
+				},
+				deps: []graph.Edge[core.Resource]{
+					{
+						Source:      bucket,
+						Destination: obj1,
+					},
+					{
+						Source:      bucket,
+						Destination: obj2,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
+			aws := AWS{
+				Config:                  &tt.cfg,
+				ConstructIdToResourceId: make(map[string]string),
+			}
+			dag := core.NewResourceGraph()
+
+			if tt.indexDocument != "" {
+				unit.IndexDocument = tt.indexDocument
+			}
+
+			err := aws.GenerateStaticUnitResources(unit, dag)
+			if tt.want.err {
+				assert.Error(err)
+				return
+			}
+			if !assert.NoError(err) {
+				return
+			}
+			for _, node := range tt.want.nodes {
+				found := false
+				for _, res := range dag.ListResources() {
+					if res.Id() == node.Id() {
+						found = true
+					}
+					if res.Id() == bucket.Id() && tt.indexDocument != "" {
+						if b, ok := res.(*s3.S3Bucket); ok {
+							assert.Equal(b.IndexDocument, tt.indexDocument)
+						}
+					}
+				}
+				assert.True(found)
+			}
+
+			for _, dep := range tt.want.deps {
+				found := false
+				for _, res := range dag.ListDependencies() {
+					if res.Source.Id() == dep.Source.Id() && res.Destination.Id() == dep.Destination.Id() {
+						found = true
+					}
+				}
+				assert.True(found)
+			}
+		})
+
+	}
+}
--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -1,3 +1,22 @@
+// Package Provider provides the interface for providers which satisfy the [compiler.ProviderPlugin].
+//
+// A Provider is a centralized location containing all necessary logic to transform an architecture, representated as klotho constructs,
+// into the necessary resources to run on the given provider and achieve the same functionality.
+//
+// In addition to implementing the [compiler.ProviderPlugin] interface, a provider must:
+//   - Provide mappings of a Kind of resource (ex. Fs or Execution Unit) to the types supported in the provider (can be services, such as s3 or lambda)
+//   - Provide a Default configuration for all of the specified types that the provider offers
+//
+// The Provider Plugins are responsible for translating the [core.ConstructGraph] into a [core.ResourceGraph] with the necessary resources defined by each provider.
+// Each specific provider is responsible for generating their own internal representation's of their resources as a [core.Resource]
+//
+// These internal representations are what will eventually be used by the [compiler.IaCPlugin] and their fields can be parsed if they meet the following criteria
+//   - They are a native Go Type
+//   - They satisfy the core.Resource interface
+//   - They are a core.IaCValue
+//   - The struct field is tagged with `render: "document`
+//
+// The `render: "document` signals that the field within the struct contains other nested fields and needs to be rendered as an object, native to the language the IaCPlugin uses
 package provider
 
 import (
