fbc6ac16daa4a964a7b0ac4ac74c253f4420b2d1
gordon-klotho <gordon.goetz@klo.dev>
2023-04-04 16:31:12 -0400

    Add coretesting package godoc

---
+++ pkg/core/coretesting/package.go
@@ -0,0 +1,3 @@
+// coretesting contains utilities to make testing things involving the `core` types and functions easier.
+// This is a separate package from core so it can be depended on only by tests and therefore pruned in non-test builds.
+package coretesting
