6a91543ee90abc3c777ab3ba5b9420048381313b
Jordan Singer <jordan.singer@klo.dev>
2023-04-04 12:15:39 -0500

    fix nat gateway template

--- pkg/infra/iac2/templates/nat_gateway/factory.ts
+++ pkg/infra/iac2/templates/nat_gateway/factory.ts
@@ -8,7 +8,7 @@ interface Args {
 
 // noinspection JSUnusedLocalSymbols
 function create(args: Args): aws.ec2.NatGateway {
-    return new aws.ec2.NatGateway('example', {
+    return new aws.ec2.NatGateway(args.Name, {
         allocationId: args.ElasticIp.id,
         subnetId: args.Subnet.id,
     })
