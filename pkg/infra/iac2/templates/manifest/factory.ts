06ea5d6933d2d86d45d43c25bb13d04be879e54f
jhsinger-klotho <111291520+jhsinger-klotho@users.noreply.github.com>
2023-04-13 18:26:12 -0400

    setting up eks logging for fargate and fluent bit (#508)

---
+++ pkg/infra/iac2/templates/manifest/factory.ts
@@ -0,0 +1,29 @@
+import * as pulumi from '@pulumi/pulumi'
+import * as pulumi_k8s from '@pulumi/kubernetes'
+
+interface Args {
+    Name: string
+    FilePath: string
+    Transformations?: Record<string, pulumi.Output<string>>
+    dependsOn?: pulumi.Input<pulumi.Input<pulumi.Resource>[]> | pulumi.Input<pulumi.Resource>
+}
+
+// noinspection JSUnusedLocalSymbols
+function create(args: Args): pulumi_k8s.yaml.ConfigFile {
+    return new pulumi_k8s.yaml.ConfigFile(
+        args.Name,
+        {
+            file: args.FilePath,
+            //TMPL {{- if .Transformations.Raw }}
+            transformations: [
+                (obj: any, opts: pulumi.CustomResourceOptions) => {
+                    //TMPL {{- range $key, $value := .Transformations.Raw }}
+                    //TMPL obj.{{ $key }} = {{ handleIaCValue $value }}
+                    //TMPL {{- end }}
+                },
+            ],
+            //TMPL {{- end }}
+        },
+        { dependsOn: args.dependsOn }
+    )
+}
