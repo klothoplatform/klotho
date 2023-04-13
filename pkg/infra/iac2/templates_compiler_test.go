7f884bd225bb8c43dde46b76ac52d76403b1fa07
jhsinger-klotho <111291520+jhsinger-klotho@users.noreply.github.com>
2023-04-13 09:33:06 -0400

    remove all render tags and add parsVal function back for security gro… (#497)

--- pkg/infra/iac2/templates_compiler_test.go
+++ pkg/infra/iac2/templates_compiler_test.go
@@ -166,7 +166,7 @@ func TestResolveStructInput(t *testing.T) {
 			}
 			resourceVal := reflect.ValueOf(tt.parentResource)
 			val := reflect.ValueOf(tt.value)
-			actual, err := tc.resolveStructInput(&resourceVal, val, tt.useDoubleQuotedStrings, "", nil)
+			actual, err := tc.resolveStructInput(&resourceVal, val, tt.useDoubleQuotedStrings, nil)
 			assert.NoError(err)
 			assert.Equal(tt.want, actual)
 		})
@@ -280,8 +280,8 @@ type (
 		id             string
 		Fizz           *DummyFizz
 		Buzz           DummyBuzz
-		NestedDoc      *NestedResource `render:"document"`
-		NestedTemplate *NestedTemplate `render:"template"`
+		NestedDoc      *NestedResource
+		NestedTemplate *NestedTemplate
 	}
 
 	NestedResource struct {
