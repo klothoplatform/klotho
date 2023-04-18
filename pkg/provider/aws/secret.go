f5a9e7abae344c62f279e7db1b0be678322088fc
gordon-klotho <gordon.goetz@klo.dev>
2023-04-17 21:16:17 -0400

    Factor generate secret to new function used by secrets and config

--- pkg/provider/aws/secret.go
+++ pkg/provider/aws/secret.go
@@ -2,45 +2,50 @@ package aws
 
 import (
 	"github.com/klothoplatform/klotho/pkg/core"
+	"github.com/klothoplatform/klotho/pkg/multierr"
 	"github.com/klothoplatform/klotho/pkg/provider/aws/resources"
 	"github.com/pkg/errors"
 )
 
 func (a *AWS) GenerateSecretsResources(construct *core.Secrets, result *core.ConstructGraph, dag *core.ResourceGraph) error {
-	for _, single := range construct.Secrets {
-		secret := resources.NewSecret(construct.Provenance(), single, a.Config.AppName)
-		dag.AddResource(secret)
-		a.MapResourceDirectlyToConstruct(secret, construct)
+	var merr multierr.Error
+	for _, secretName := range construct.Secrets {
+		merr.Append(a.generateSecret(construct, result, dag, secretName))
+	}
+	return merr.ErrOrNil()
+}
 
-		secretVersion := resources.NewSecretVersion(secret, single)
-		dag.AddResource(secretVersion)
-		dag.AddDependency(secretVersion, secret)
+func (a *AWS) generateSecret(construct core.Construct, result *core.ConstructGraph, dag *core.ResourceGraph, secretName string) error {
+	secret := resources.NewSecret(construct.Provenance(), secretName, a.Config.AppName)
+	dag.AddResource(secret)
+	a.MapResourceDirectlyToConstruct(secret, construct)
 
-		for _, upstreamCons := range result.GetUpstreamConstructs(construct) {
-			unit, isUnit := upstreamCons.(*core.ExecutionUnit)
-			if !isUnit {
-				continue
-			}
+	secretVersion := resources.NewSecretVersion(secret, secretName)
+	dag.AddDependenciesReflect(secretVersion)
 
-			actions := []string{`secretsmanager:DescribeSecret`, `secretsmanager:GetSecretValue`}
-			policyResources := []core.IaCValue{{
-				Resource: secret,
-				Property: resources.ARN_IAC_VALUE,
-			}}
-			policyDoc := resources.CreateAllowPolicyDocument(actions, policyResources)
-			policy := resources.NewIamPolicy(a.Config.AppName, construct.Id(), construct.Provenance(), policyDoc)
-			if res := dag.GetResource(policy.Id()); res != nil {
-				if existingPolicy, ok := res.(*resources.IamPolicy); ok {
-					existingPolicy.Policy.Statement = append(existingPolicy.Policy.Statement, policyDoc.Statement...)
-					dag.AddDependency(existingPolicy, secretVersion)
-				} else {
-					return errors.Errorf("expected resource with id, %s, to be an iam policy", res.Id())
-				}
+	for _, upstreamCons := range result.GetUpstreamConstructs(construct) {
+		unit, isUnit := upstreamCons.(*core.ExecutionUnit)
+		if !isUnit {
+			continue
+		}
+
+		actions := []string{`secretsmanager:DescribeSecret`, `secretsmanager:GetSecretValue`}
+		policyResources := []core.IaCValue{{
+			Resource: secret,
+			Property: resources.ARN_IAC_VALUE,
+		}}
+		policyDoc := resources.CreateAllowPolicyDocument(actions, policyResources)
+		policy := resources.NewIamPolicy(a.Config.AppName, construct.Id(), construct.Provenance(), policyDoc)
+		if res := dag.GetResource(policy.Id()); res != nil {
+			if existingPolicy, ok := res.(*resources.IamPolicy); ok {
+				existingPolicy.Policy.Statement = append(existingPolicy.Policy.Statement, policyDoc.Statement...)
+				dag.AddDependency(existingPolicy, secret)
 			} else {
-				dag.AddResource(policy)
-				dag.AddDependency(policy, secretVersion)
-				a.PolicyGenerator.AddAllowPolicyToUnit(unit.Id(), policy)
+				return errors.Errorf("expected resource with id, %s, to be an iam policy", res.Id())
 			}
+		} else {
+			dag.AddDependenciesReflect(policy)
+			a.PolicyGenerator.AddAllowPolicyToUnit(unit.Id(), policy)
 		}
 	}
 	return nil
