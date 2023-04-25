77d7ac95f79aedc6e123c500adf61a58dc0118b1
DavidSeptimus-Klotho <david.septimus@klo.dev>
2023-04-25 14:30:40 -0600

    Cleans up API Gateway templates and adds missing parent relationships

--- pkg/infra/iac2/templates/api_method/factory.ts
+++ pkg/infra/iac2/templates/api_method/factory.ts
@@ -11,15 +11,27 @@ interface Args {
 
 // noinspection JSUnusedLocalSymbols
 function create(args: Args): aws.apigateway.Method {
-    return new aws.apigateway.Method(args.Name, {
-        restApi: args.RestApi.id,
-        //TMPL {{- if .Resource.Raw }}
-        resourceId: args.Resource.id,
-        //TMPL {{- else}}
-        //TMPL resourceId: args.RestApi.rootResourceId,
-        //TMPL {{- end }}
-        httpMethod: args.HttpMethod,
-        authorization: args.Authorization,
-        requestParameters: args.RequestParameters,
-    })
+    return new aws.apigateway.Method(
+        args.Name,
+        {
+            restApi: args.RestApi.id,
+            //TMPL {{- if .Resource.Raw }}
+            resourceId: args.Resource.id,
+            //TMPL {{- else}}
+            //TMPL resourceId: args.RestApi.rootResourceId,
+            //TMPL {{- end }}
+            httpMethod: args.HttpMethod,
+            authorization: args.Authorization,
+            //TMPL {{- if .RequestParameters.Raw }}
+            requestParameters: args.RequestParameters,
+            //TMPL {{- end }}
+        },
+        {
+            //TMPL {{- if .Resource.Raw }}
+            parent: args.Resource,
+            //TMPL {{- else }}
+            //TMPL parent: args.RestApi
+            //TMPL {{- end }}
+        }
+    )
 }
