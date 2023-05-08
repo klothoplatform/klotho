9b6f2cc9f3a3c57c390f104929f51edbf71702e9
gordon-klotho <gordon.goetz@klo.dev>
2023-05-08 19:55:23 -0400

    Add some comments / godoc

--- pkg/provider/imports/plugin.go
+++ pkg/provider/imports/plugin.go
@@ -6,6 +6,10 @@ import (
 	"go.uber.org/zap"
 )
 
+// Plugin is responsible for adding the `Imported` resource to the graph, and adding a dependency from the resource
+// which is to be replaced with the import. This mechanism for importing will work for all providers, so it is accomplished
+// here in a generic plugin to prevent unnecessary reimplementation.
+// If any special coordinating logic is required for a specific provider or resource, that should be implemented in the provider plugin.
 type Plugin struct {
 	Config *config.Application
 }
