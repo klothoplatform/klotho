9a2e617f172abf02da138a274933cb587a0f8574
Jordan Singer <jordan.singer@klo.dev>
2023-05-22 20:55:22 -0400

    add expose expansion method

--- pkg/provider/aws/expose_test.go
+++ pkg/provider/aws/expose_test.go
@@ -13,6 +13,63 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_ExpandExpose(t *testing.T) {
+	gw := &core.Gateway{AnnotationKey: core.AnnotationKey{ID: "test", Capability: annotation.ExecutionUnitCapability}}
+	cases := []struct {
+		name    string
+		gw      *core.Gateway
+		config  *config.Application
+		want    coretesting.ResourcesExpectation
+		wantErr bool
+	}{
+		{
+			name:   "single expose",
+			gw:     gw,
+			config: &config.Application{AppName: "my-app", Defaults: config.Defaults{Expose: config.KindDefaults{Type: ApiGateway}}},
+			want: coretesting.ResourcesExpectation{
+				Nodes: []string{
+					"aws:api_deployment:my-app-test",
+					"aws:api_stage:my-app-test",
+					"aws:rest_api:my-app-test",
+				},
+				Deps: []coretesting.StringDep{
+					{Source: "aws:api_deployment:my-app-test", Destination: "aws:rest_api:my-app-test"},
+					{Source: "aws:api_stage:my-app-test", Destination: "aws:api_deployment:my-app-test"},
+					{Source: "aws:api_stage:my-app-test", Destination: "aws:rest_api:my-app-test"},
+				},
+			},
+		},
+		{
+			name:    "unsupported type",
+			gw:      gw,
+			config:  &config.Application{AppName: "my-app", Defaults: config.Defaults{Expose: config.KindDefaults{Type: Lambda}}},
+			wantErr: true,
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
+			dag := core.NewResourceGraph()
+
+			aws := AWS{
+				Config: tt.config,
+			}
+			err := aws.expandExpose(dag, tt.gw)
+
+			if tt.wantErr {
+				assert.Error(err)
+				return
+			}
+			if !assert.NoError(err) {
+				return
+			}
+			tt.want.Assert(t, dag)
+			resource := aws.GetResourceTiedToConstruct(tt.gw)
+			assert.NotNil(resource)
+		})
+	}
+}
+
 func Test_CreateRestApi(t *testing.T) {
 	appName := "test"
 	unit1 := &core.ExecutionUnit{
