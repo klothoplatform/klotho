07fb5fc1c22cf93e5b2819216ef348087b285a00
Yuval Shavit <yuval.shavit@klo.dev>
2023-05-25 15:06:55 -0400

    checkpoint: s3 objects done(?)

--- pkg/provider/aws/static_unit.go
+++ pkg/provider/aws/static_unit.go
@@ -4,20 +4,42 @@ import (
 	"path/filepath"
 
 	"github.com/klothoplatform/klotho/pkg/core"
+	"github.com/klothoplatform/klotho/pkg/multierr"
 	"github.com/klothoplatform/klotho/pkg/provider/aws/resources"
 )
 
-func (a *AWS) GenerateStaticUnitResources(unit *core.StaticUnit, dag *core.ResourceGraph) error {
+//func (a *AWS) GenerateStaticUnitResources(unit *core.StaticUnit, dag *core.ResourceGraph) error {
+//
+//	bucket := resources.NewS3Bucket(unit, a.Config.AppName)
+//	bucket.IndexDocument = unit.IndexDocument
+//	dag.AddResource(bucket)
+//	for _, f := range unit.Files() {
+//		object := resources.NewS3Object(bucket, filepath.Base(f.Path()), f.Path(), filepath.Join(unit.ID, f.Path()))
+//		dag.AddResource(object)
+//		dag.AddDependency(object, bucket)
+//		a.MapResourceDirectlyToConstruct(object, unit)
+//	}
+//	a.MapResourceDirectlyToConstruct(bucket, unit)
+//	return nil
+//}
 
-	bucket := resources.NewS3Bucket(unit, a.Config.AppName)
-	bucket.IndexDocument = unit.IndexDocument
-	dag.AddResource(bucket)
+func (a *AWS) expandStaticUnit(dag *core.ResourceGraph, unit *core.StaticUnit) error {
+	errs := multierr.Error{}
 	for _, f := range unit.Files() {
-		object := resources.NewS3Object(bucket, filepath.Base(f.Path()), f.Path(), filepath.Join(unit.ID, f.Path()))
-		dag.AddResource(object)
-		dag.AddDependency(object, bucket)
-		a.MapResourceDirectlyToConstruct(object, unit)
+		object, err := core.CreateResource[*resources.S3Object](dag, resources.S3ObjectCreateParams{
+			AppName:  a.Config.AppName,
+			Refs:     core.AnnotationKeySetOf(unit.AnnotationKey),
+			UnitName: unit.Provenance().ID,
+			Name:     filepath.Base(f.Path()),
+			Key:      f.Path(),
+			FilePath: filepath.Join(unit.ID, f.Path()),
+		})
+		if err != nil {
+			errs.Append(err)
+			continue
+		}
+		err = a.MapResourceToConstruct(object, unit)
+		errs.Append(err)
 	}
-	a.MapResourceDirectlyToConstruct(bucket, unit)
-	return nil
+	return errs.ErrOrNil()
 }
