81c0c50a94f8a032fa46a0622daa638df9b62966
Jordan Singer <jordan.singer@klo.dev>
2023-10-05 12:10:58 -0500

    default image name for ecr

--- pkg/infra/iac3/templates/aws/iam_role/factory.ts
+++ pkg/infra/iac3/templates/aws/iam_role/factory.ts
@@ -1,11 +1,11 @@
 import * as aws from '@pulumi/aws'
 import * as awsInputs from '@pulumi/aws/types/input'
 import * as pulumi from '@pulumi/pulumi'
-import { TemplateWrapper } from '../../wrappers'
+import { TemplateWrapper, ModelCaseWrapper } from '../../wrappers'
 
 interface Args {
     Name: string
-    AssumeRolePolicyDoc: string
+    AssumeRolePolicyDoc: ModelCaseWrapper<string>
     InlinePolicies: TemplateWrapper<pulumi.Input<pulumi.Input<awsInputs.iam.RoleInlinePolicy>[]>>
     ManagedPolicies: pulumi.Output<string>[]
     AwsManagedPolicies: string[]
