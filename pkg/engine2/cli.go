41f7ce6c867181b1cc7c7a32b8f1be2148809f6b
Gordon <gordon.goetz@klo.dev>
2023-12-22 14:09:49 -0500

    Add important to property template

--- pkg/engine2/cli.go
+++ pkg/engine2/cli.go
@@ -181,6 +181,7 @@ func addSubProperties(properties map[string]any, subProperties map[string]knowle
 			"configurationDisabled": details.ConfigurationDisabled,
 			"required":              details.Required,
 			"description":           details.Description,
+			"important":             details.IsImportant,
 		}
 		for _, validationField := range validationFields {
 			valField := reflect.ValueOf(subProperty).Elem().FieldByName(validationField)
