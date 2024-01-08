57cf69c0a8751fd34d26079842d7437a93079fc3
Gordon <gordon.goetz@klo.dev>
2024-01-08 17:02:12 -0500

    Add unit test

--- pkg/knowledge_base2/graph.go
+++ pkg/knowledge_base2/graph.go
@@ -12,9 +12,10 @@ import (
 
 type (
 	// DependencyLayer represents how far away a resource to return for the [Upstream]/[Downstream] methods.
-	// 1. ResourceLocalLayer (layer 1) represents any unique resources the target resource needs to be operational
+	// 1. ResourceLocalLayer (layer 1) represents any unique resources the target resource needs to be operational,
+	//    transitively.
 	// 2. ResourceGlueLayer (layer 2) represents all upstream/downstream resources that represent glue.
-	//  This will not include any other functional resources and will stop searching paths
+	//  This will not include any other functional resources and will stopsearching paths
 	//  once a functional resource is reached.
 	// 3. FirstFunctionalLayer (layer 3) represents all upstream/downstream resources that represent glue and
 	//  the first functional resource in other paths from the target resource.
@@ -42,40 +43,6 @@ func resourceLocal(
 	ids *[]construct.ResourceId,
 ) graph_addons.WalkGraphFunc[construct.ResourceId] {
 	return func(path graph_addons.Path[construct.ResourceId], nerr error) error {
-		id := path[len(path)-1]
-		isSideEffect, err := IsOperationalResourceSideEffect(dag, kb, rid, id)
-		if err != nil {
-			return errors.Join(nerr, err)
-		}
-		if isSideEffect {
-			if ids != nil {
-				(*ids) = append(*ids, id)
-			}
-			return nil
-		}
-		return graph_addons.SkipPath
-	}
-}
-
-func resourceDirect(
-	dag construct.Graph,
-	ids *[]construct.ResourceId,
-) graph_addons.WalkGraphFunc[construct.ResourceId] {
-	return func(path graph_addons.Path[construct.ResourceId], nerr error) error {
-		id := path[len(path)-1]
-		if ids != nil {
-			(*ids) = append(*ids, id)
-		}
-		return graph_addons.SkipPath
-	}
-}
-
-func resourceGlue(
-	dag construct.Graph,
-	kb TemplateKB,
-	ids *[]construct.ResourceId,
-) graph_addons.WalkGraphFunc[construct.ResourceId] {
-	return func(path graph_addons.Path[construct.ResourceId], nerr error) error {
 		if len(path) <= 1 {
 			// skip source, this shouldn't happen but just in case
 			return nil
@@ -93,6 +60,35 @@ func resourceGlue(
 		}
 		(*ids) = append(*ids, last)
 		return nil
+	}
+}
+
+func resourceDirect(
+	dag construct.Graph,
+	ids *[]construct.ResourceId,
+) graph_addons.WalkGraphFunc[construct.ResourceId] {
+	return func(path graph_addons.Path[construct.ResourceId], nerr error) error {
+		id := path[len(path)-1]
+		if ids != nil {
+			(*ids) = append(*ids, id)
+		}
+		return graph_addons.SkipPath
+	}
+}
+
+func resourceGlue(
+	kb TemplateKB,
+	ids *[]construct.ResourceId,
+) graph_addons.WalkGraphFunc[construct.ResourceId] {
+	return func(path graph_addons.Path[construct.ResourceId], nerr error) error {
+		id := path[len(path)-1]
+		if GetFunctionality(kb, id) == Unknown {
+			if ids != nil {
+				(*ids) = append(*ids, id)
+			}
+			return nil
+		}
+		return graph_addons.SkipPath
 	}
 }
 
@@ -185,7 +181,7 @@ func Downstream(dag construct.Graph, kb TemplateKB, rid construct.ResourceId, la
 		}
 		return ids, nil
 	case ResourceGlueLayer:
-		f = resourceGlue(dag, kb, &result)
+		f = resourceGlue(kb, &result)
 	case FirstFunctionalLayer:
 		f = firstFunctional(kb, &result)
 	case AllDepsLayer:
@@ -230,7 +226,7 @@ func Upstream(dag construct.Graph, kb TemplateKB, rid construct.ResourceId, laye
 		}
 		return ids, nil
 	case ResourceGlueLayer:
-		f = resourceGlue(dag, kb, &result)
+		f = resourceGlue(kb, &result)
 	case FirstFunctionalLayer:
 		f = firstFunctional(kb, &result)
 	case AllDepsLayer:
@@ -255,7 +251,7 @@ func layerWalkFunc(
 	case ResourceDirectLayer:
 		return resourceDirect(dag, &result), nil
 	case ResourceGlueLayer:
-		return resourceGlue(dag, kb, &result), nil
+		return resourceGlue(kb, &result), nil
 	case FirstFunctionalLayer:
 		return firstFunctional(kb, &result), nil
 	case AllDepsLayer:
@@ -316,40 +312,42 @@ func IsOperationalResourceSideEffect(dag construct.Graph, kb TemplateKB, rid, si
 			}
 		}
 
+		if !ruleSatisfied {
+			continue
+		}
+
 		// If the side effect resource fits the rule we then perform 2 more checks
 		// 1. is there a path in the direction of the rule
 		// 2. Is the property set with the resource that we are checking for
-		if ruleSatisfied {
-			if step.Direction == DirectionUpstream {
-				resources, err := graph.ShortestPathStable(dag, sideEffect, rid, construct.ResourceIdLess)
-				if len(resources) == 0 || err != nil {
-					continue
-				}
-			} else {
-				resources, err := graph.ShortestPathStable(dag, rid, sideEffect, construct.ResourceIdLess)
-				if len(resources) == 0 || err != nil {
-					continue
-				}
-			}
-
-			propertyVal, err := resource.GetProperty(property.Details().Path)
-			if err != nil || propertyVal == nil {
+		if step.Direction == DirectionUpstream {
+			resources, err := graph.ShortestPathStable(dag, sideEffect, rid, construct.ResourceIdLess)
+			if len(resources) == 0 || err != nil {
 				continue
 			}
-			val := reflect.ValueOf(propertyVal)
-			if val.Kind() == reflect.Array || val.Kind() == reflect.Slice {
-				for i := 0; i < val.Len(); i++ {
-					if arrId, ok := val.Index(i).Interface().(construct.ResourceId); ok && arrId == sideEffect {
-						return true, nil
-					}
-				}
-			} else {
-				if val.IsZero() {
-					continue
-				}
-				if valId, ok := val.Interface().(construct.ResourceId); ok && valId == sideEffect {
+		} else {
+			resources, err := graph.ShortestPathStable(dag, rid, sideEffect, construct.ResourceIdLess)
+			if len(resources) == 0 || err != nil {
+				continue
+			}
+		}
+
+		propertyVal, err := resource.GetProperty(property.Details().Path)
+		if err != nil || propertyVal == nil {
+			continue
+		}
+		val := reflect.ValueOf(propertyVal)
+		if val.Kind() == reflect.Array || val.Kind() == reflect.Slice {
+			for i := 0; i < val.Len(); i++ {
+				if arrId, ok := val.Index(i).Interface().(construct.ResourceId); ok && arrId == sideEffect {
 					return true, nil
 				}
+			}
+		} else {
+			if val.IsZero() {
+				continue
+			}
+			if valId, ok := val.Interface().(construct.ResourceId); ok && valId == sideEffect {
+				return true, nil
 			}
 		}
 
