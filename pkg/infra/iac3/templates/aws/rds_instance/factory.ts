51e31ccce76cdfa1f19a27e4478b368d64936d33
Jordan Singer <jordan.singer@klo.dev>
2024-01-19 16:20:52 -0500

    add host and port

--- pkg/infra/iac3/templates/aws/rds_instance/factory.ts
+++ pkg/infra/iac3/templates/aws/rds_instance/factory.ts
@@ -50,6 +50,8 @@ function properties(object: aws.rds.Instance, args: Args) {
         RdsConnectionArn: pulumi.interpolate`arn:aws:rds-db:${region.name}:${accountId.accountId}:dbuser:${object.resourceId}/${object.username}`,
         Endpoint: object.endpoint,
         ConnectionString: pulumi.interpolate`${args.Engine}://${object.username}:${object.password}@${object.endpoint}/${args.DatabaseName}`,
+        Host: object.address.apply((address) => address.split(':')[0]),
+        Port: object.address.apply((address) => address.split(':')[1]),
     }
 }
 
