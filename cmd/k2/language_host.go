47c1e65984ea1f8a5b1a899cddfe66d539d0b9b1
Gordon <gordon.goetz@klo.dev>
2024-06-10 16:18:30 -0400

    Setpgid on the python subprocess

--- cmd/k2/language_host.go
+++ cmd/k2/language_host.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 	"time"
 
 	pb "github.com/klothoplatform/klotho/pkg/k2/language_host/go"
@@ -42,6 +43,8 @@ func startPythonClient() *exec.Cmd {
 	cmd.Dir = "pkg/k2/language_host/python"
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	// spawn the python process as a subprocess of the CLI so it is guaranteed to be killed when the CLI exits
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 
 	if err := cmd.Start(); err != nil {
 		zap.S().Fatalf("failed to start Python client: %v", err)
@@ -50,7 +53,11 @@ func startPythonClient() *exec.Cmd {
 
 	go func() {
 		err := cmd.Wait()
-		zap.S().Infof("Python client exited, err: %v", err)
+		if err != nil {
+			zap.S().Errorf("Python process exited with error: %v", err)
+		} else {
+			zap.L().Debug("Python process exited successfully")
+		}
 	}()
 	return cmd
 }
