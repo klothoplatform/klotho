43163038085faba0c6bbf39f9e8ad4d9956a45bf
DavidSeptimus-Klotho <david.septimus@klo.dev>
2024-06-10 16:24:46 -0600

    Resolves a TS issue when a klotho resource doesn't have a URN

--- pkg/infra/iac/plugin.go
+++ pkg/infra/iac/plugin.go
@@ -170,7 +170,7 @@ func renderStackOutputs(tc *TemplatesCompiler, buf *bytes.Buffer, outputs map[st
 func renderUrnMap(buf *bytes.Buffer, vars variables) {
 	buf.WriteString("export const $urns = {\n")
 	for id, obj := range vars {
-		buf.WriteString(fmt.Sprintf("\t\"%s\": %s.urn,\n", id, obj))
+		buf.WriteString(fmt.Sprintf("\t\"%s\": (%s as any).urn,\n", id, obj))
 	}
 	buf.WriteString("}\n")
 }
