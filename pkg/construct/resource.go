6d10302bb5326471e960308e611daddf8921b9ce
Gordon <gordon.goetz@klo.dev>
2024-07-15 15:00:07 -0400

    WIP

--- pkg/construct/resource.go
+++ pkg/construct/resource.go
@@ -5,3 +5,14 @@ type Resource struct {
 	Properties Properties
 	Imported   bool
 }
+
+func (r Resource) Equals(other any) bool {
+	switch other := other.(type) {
+	case Resource:
+		return r.ID == other.ID && r.Properties.Equals(other.Properties)
+	case *Resource:
+		return r.ID == other.ID && r.Properties.Equals(other.Properties)
+	default:
+		return false
+	}
+}
