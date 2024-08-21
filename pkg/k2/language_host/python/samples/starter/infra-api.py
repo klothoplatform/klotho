4660cd5480eae38f130ac94589d23c22c47991e6
Gordon <gordon.goetz@klo.dev>
2024-08-21 11:09:17 -0400

    Use unique app+project configuration for sample apps

--- pkg/k2/language_host/python/samples/starter/infra-api.py
+++ pkg/k2/language_host/python/samples/starter/infra-api.py
@@ -1,5 +1,21 @@
-import infra
+import os
+from pathlib import Path
+
+import klotho
 import klotho.aws as aws
 
+app = klotho.Application(
+    "api",
+    project=os.getenv("PROJECT_NAME", "starter1"),
+)
+
+dir = Path(__file__).parent.absolute()
+
+container = aws.Container(
+    "my-container",
+    dockerfile=str(dir / "Dockerfile"),
+    context=str(dir),
+)
+
 api = aws.Api("my-api")
-api.route_to("/", infra.container)
+api.route_to("/", container)
--- pkg/k2/language_host/python/samples/starter/infra.py
+++ pkg/k2/language_host/python/samples/starter/infra.py
@@ -6,15 +6,8 @@ import klotho.aws as aws
 
 # Create the Application instance
 app = klotho.Application(
-    "my-app",
-    project=os.getenv(
-        "PROJECT_NAME", "my-project"
-    ),  # Default to 'my-project' or the environment variable value
-    environment=os.getenv("KLOTHO_ENVIRONMENT", "default"),
-    # Default to 'default' or the environment variable value
-    default_region=os.getenv(
-        "AWS_REGION", "us-west-2"
-    ),  # Default to 'us-east-1' or the environment variable value
+    "basic",
+    project=os.getenv("PROJECT_NAME", "starter"),
 )
 
 dir = Path(__file__).parent.absolute()
